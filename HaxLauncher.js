d412403c538e47e6f7ab2062abe03b7f38fa715e
CCHaxDev <104091357+CCHaxDev@users.noreply.github.com>
2022-04-21 10:55:36 -0400

    save #1

--- HaxLauncher.js
+++ HaxLauncher.js
@@ -1,3 +1,15 @@
 javascript: {
-	
+    async function getClient() {
+        try {
+            let res = await fetch("https://pastebin.com/raw/0BnNh59B");
+            return await res.text();
+        }
+        catch(e) {
+            alert(e);
+        }
+    }
+
+    alert("running");
+    
+    getClient();
 }
